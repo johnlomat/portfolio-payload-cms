8d1c83b0138fd830c7b4ede5a5d240dd0678fd2c
johnlomat <edwardlomat1503@gmail.com>
2024-08-03 21:53:25 +0800

    Added Payload CMS SEO plugin and custom keywords field

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,12 +6,15 @@ import { webpackBundler } from '@payloadcms/bundler-webpack'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import { buildConfig } from 'payload/config'
 import cloudinaryPlugin from 'payload-cloudinary-plugin/dist/plugins'
+import seoPlugin from '@payloadcms/plugin-seo'
 
 import Users from './collections/Users'
+import Pages from './collections/Pages'
 import Projects from './collections/Projects'
 import Skills from './collections/Skills'
 import Technologies from './collections/Technologies'
 import { Media } from './collections/Media'
+import KeywordsField from './fields/payload-seo-plugin/keywords-field'
 
 export default buildConfig({
   admin: {
@@ -20,7 +23,7 @@ export default buildConfig({
     css: path.resolve(__dirname, 'styles/custom-style-admin.scss'),
   },
   editor: slateEditor({}),
-  collections: [Users, Projects, Skills, Technologies, Media],
+  collections: [Users, Pages, Projects, Skills, Technologies, Media],
   cors: ['http://localhost:3000', process.env.NEXT_PUBLIC_SITE_URL],
   csrf: ['http://localhost:3000', process.env.NEXT_PUBLIC_SITE_URL],
   typescript: {
@@ -29,7 +32,16 @@ export default buildConfig({
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
-  plugins: [payloadCloud(), cloudinaryPlugin()],
+  plugins: [
+    payloadCloud(),
+    cloudinaryPlugin(),
+    seoPlugin({
+      collections: ['pages'],
+      uploadsCollection: 'media',
+      generateTitle: ({ doc }: { doc: any }) => `${doc.title.value} - John Lomat`,
+      fields: [KeywordsField],
+    }),
+  ],
   db: mongooseAdapter({
     url: process.env.DATABASE_URI,
   }),
